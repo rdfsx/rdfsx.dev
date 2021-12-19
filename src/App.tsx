04e480d4a0b0aca0c7db36a2f20fc45abd11ede2
rdfsx <m315498@gmail.com>
2021-12-19 11:50:46 +0300

    :construction: updated bio

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function Page() {
             <header className="App-header">
                 <article className="App-article">
                     <h2>Mikhail Anishchanka</h2>
-                    <Paragraph text1={"bio:"} text2={"web developer, BSc ITE"} />
+                    <Paragraph text1={"bio:"} text2={"Python Developer at LeverX Group, BSc ITE"} />
                     <Paragraph text1={"stack:"} text2={"Python, NodeJS, TypeScript, C++/Qt, React, PostgreSQL, MongoDB, Docker"} />
                     <Paragraph text1={"code:"} text2={"GitHub"} link={"https://github.com/rdfsx"} />
                     <Paragraph text1={"contact:"} text2={"Telegram"} link={"https://t.me/rdfsx"} />
