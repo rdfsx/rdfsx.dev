8a2d69e5914a83cea1e2c6be0f58f2c51ab32956
rdfsx <m315498@gmail.com>
2022-05-21 14:42:53 +0300

    added smth

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,15 @@ function Page() {
             <header className="App-header">
                 <article className="App-article">
                     <h2>Mikhail Anishchanka</h2>
-                    <Paragraph text1={"bio:"} text2={"Python Developer at LeverX Group, BSc ITE"} />
+                    <Paragraph text1={"bio:"} text2={"Software Engineer at LeverX Group, BSc ITE"} />
                     <Paragraph text1={"stack:"} text2={"Python, NodeJS, TypeScript, C++/Qt, React, PostgreSQL, MongoDB, Docker"} />
                     <Paragraph text1={"code:"} text2={"GitHub"} link={"https://github.com/rdfsx"} />
                     <Paragraph text1={"contact:"} text2={"Telegram"} link={"https://t.me/rdfsx"} />
                     <Paragraph text1={"social media:"} text2={"Linkedin"}
                                link={"https://linkedin.com/in/mikhail-anishchanka"} />
-                    <Paragraph text1={"playlist:"} text2={"Spotify"}
-                               link={"https://open.spotify.com/user/27urfgjsrtgszbucbb8wxnyo8?si=ffc3f0443335450f"} />
+                    <Paragraph text1={"english:"} text2={"B1"} />
                     <br />
-                    <h5>© 2021 | website is under construction</h5>
+                    <h5>© 2022 | website is under construction</h5>
                 </article>
             </header>
         </div>
